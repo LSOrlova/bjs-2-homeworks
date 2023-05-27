ca561e6f2171c5d09f62e7e51abe30e941edd647
Liubov Orlova <118666858+LSOrlova@users.noreply.github.com>
2023-05-27 16:09:13 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -17,35 +17,5 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  if (isNaN(percent)) {
-    return 'Параметр "Процентная ставка" содержит неправильное значение ' + "\"" + `${String(percent)}` + "\"";
-  }
-  if (isNaN(contribution)) {
-    return 'Параметр "Начальный взнос" содержит неправильное значение ' + "\"" + `${contribution}` + "\"";
-  }
-  if (isNaN(amount)) {
-    return 'Параметр "Общая стоимость" содержит неправильное значение ' + "\"" + `${amount}` + "\"";
-  }
-
-  let totalAmount;
-  let creditBody;
-  let percentDoubleMonth;
-  let payment;
-  creditBody = amount - contribution;
-  let currentDate = new Date();
-  let resultDate = 0;
-  resultDate = monthDiff(date, currentDate);
-  percentDoubleMonth = percent / 100 / 12;
-  payment = creditBody * (percentDoubleMonth + percentDoubleMonth / (((1 + percentDoubleMonth) ** resultDate) - 1));
-  totalAmount = (payment * resultDate);
-  console.log(totalAmount);
-  return + totalAmount.toFixed(2);
-}
-
-function monthDiff(d1, d2) {
-  let months;
-  months = (d1.getFullYear() - d2.getFullYear()) * 12;
-  months -= d2.getMonth();
-  months += d1.getMonth();
-  return months <= 0 ? 0 : months;
+  
 }
