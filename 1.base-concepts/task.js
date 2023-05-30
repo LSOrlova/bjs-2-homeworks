e5fa456aa385143ba140d3cfc074465394f264c9
Luyba <lsorlova82@gmail.com>
2023-05-30 12:44:04 +0300

    fixes 30052023

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,8 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = Math.pow(b, 2) - (4 * a * c);
+	let arr = [];
+	let d = Math.pow(b, 2) - (4 * a * c);
 	let x1 = (-b + Math.sqrt(d)) / (2 * a);
 	let x2 = (-b - Math.sqrt(d)) / (2 * a);
 
@@ -13,39 +14,15 @@ function solveEquation(a, b, c) {
 	} else {
 		arr = [x1, x2]
 	}
-  return arr;
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  if (isNaN(percent)) {
-    return 'Параметр "Процентная ставка" содержит неправильное значение ' + "\"" + `${String(percent)}` + "\"";
-  }
-  if (isNaN(contribution)) {
-    return 'Параметр "Начальный взнос" содержит неправильное значение ' + "\"" + `${contribution}` + "\"";
-  }
-  if (isNaN(amount)) {
-    return 'Параметр "Общая стоимость" содержит неправильное значение ' + "\"" + `${amount}` + "\"";
-  }
 
-  let totalAmount;
-  let creditBody;
-  let percentDoubleMonth;
-  let payment;
-  creditBody = amount - contribution;
-  let currentDate = new Date();
-  let resultDate = 0;
-  resultDate = monthDiff(date, currentDate);
-  percentDoubleMonth = percent / 100 / 12;
-  payment = creditBody * (percentDoubleMonth + percentDoubleMonth / (((1 + percentDoubleMonth) ** resultDate) - 1));
-  totalAmount = (payment * resultDate);
-  console.log(totalAmount);
-  return + totalAmount.toFixed(2);
-}
-
-function monthDiff(d1, d2) {
-  let months;
-  months = (d1.getFullYear() - d2.getFullYear()) * 12;
-  months -= d2.getMonth();
-  months += d1.getMonth();
-  return months <= 0 ? 0 : months;
+	let creditBody = amount - contribution;
+	let percentDoubleMonth = percent / 100 / 12;
+	let payment = creditBody * (percentDoubleMonth + percentDoubleMonth / (((1 + percentDoubleMonth) ** date) - 1));
+	let totalAmount = (payment * date);
+	console.log(totalAmount);
+	return +totalAmount.toFixed(2);
 }
